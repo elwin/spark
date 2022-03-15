642e4cd746a4d55d1e2e4733dc09b3ff3eeaf829
Elwin Stephan <elw@zurich.ibm.com>
2022-03-15 16:33:08 +0100

    add core only build

---
+++ elwin/core.sh
@@ -0,0 +1,18 @@
+set -e
+
+if [ $# -eq 0 ]; then
+  echo "Usage: core.sh [tag]"
+  exit 1
+fi
+
+REPO=us.icr.io/elwin/spark
+TAG=$1
+
+./build/mvn -DskipTests -pl :spark-core_2.12 clean package
+
+mkdir -p elwin/dist
+cp core/target/spark-core_2.12-3.2.1.jar elwin/dist/spark-core_2.12-3.2.1.jar
+
+IMAGE_NAME="${REPO}:${TAG}"
+docker build -f elwin/Dockerfile -t "${IMAGE_NAME}" elwin
+docker push "${IMAGE_NAME}"
