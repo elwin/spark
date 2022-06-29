e2e9871336154a3315fa419359f608ec2ff32017
Elwin Stephan <elw@zurich.ibm.com>
2022-06-29 17:28:54 +0200

    fix log print

--- core/src/main/scala/org/apache/spark/scheduler/cluster/CoarseGrainedSchedulerBackend.scala
+++ core/src/main/scala/org/apache/spark/scheduler/cluster/CoarseGrainedSchedulerBackend.scala
@@ -122,7 +122,7 @@ class CoarseGrainedSchedulerBackend(scheduler: TaskSchedulerImpl, val rpcEnv: Rp
     val t0 = System.nanoTime()
     val result = block
     val t1 = System.nanoTime()
-    logInfo(s"""elw3: {"type": "measurement", "name": "${name}", "duration": ${t1 - t0}, "executor_id": "${executorID}" "timestamp": $t0}""")
+    logInfo(s"""elw3: {"type": "measurement", "name": "${name}", "duration": ${t1 - t0}, "executor_id": "${executorID}", "timestamp": $t0}""")
 
     result
   }
