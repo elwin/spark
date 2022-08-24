528173a904948ba5f8ab7ee91db52c3a511428ff
Elwin Stephan <elw@zurich.ibm.com>
2022-08-24 20:55:31 +0200

    push to ibm cloud

--- elwin/base-push.sh
+++ elwin/base-push.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-REPO=ghcr.io/elwin/spark
+#REPO=ghcr.io/elwin/spark
+REPO=us.icr.io/elwin
 TAG=$1
 
 ./bin/docker-image-tool.sh -r ${REPO} -t ${TAG} build
-docker push $REPO:$TAG
+docker push $REPO:"$TAG"
+
 scp -r dist zac25:~/spark-versions/"${TAG}"
